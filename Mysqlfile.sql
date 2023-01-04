1978bbe801e47fe790a5341761e60de4706aa6bf
Tobi <118818317+tobiolatunji1@users.noreply.github.com>
2023-01-04 13:45:25 -0600

    Update Mysqlfile.sql

--- Mysqlfile.sql
+++ Mysqlfile.sql
@@ -1,4 +1,8 @@
-# SQL_projects
-Collection of Mysql files
-SELECT *
-FROM ()
+# SQL_projects #Collection of Mysql files SELECT * FROM customer 
+select
+    store_id,
+    customer_id,
+    email,
+    first_name,
+    last_name
+from customer
